547ac5418afd679503d7c87a9a6f9e4c3511bec5
XavierCuases <xaviercuases@hotmail.com>
2025-01-21 21:48:54 -0500

    feat: :fire: change dependencies and code

--- create-activity/src/database/dynamoDBClient.js
+++ create-activity/src/database/dynamoDBClient.js
@@ -1,5 +1,8 @@
+const AWS = require('aws-sdk'); // Importa aws-sdk
+
 const dynamoDB = new AWS.DynamoDB.DocumentClient({
-    region: 'us-east-1', // Región especificada
-    endpoint: 'http://localhost:8000', // Endpoint local
-  });
-  
+  region: 'us-east-1',
+  endpoint: 'http://localhost:8000', // Endpoint local para DynamoDB Local
+});
+
+module.exports = dynamoDB;
--- create-activity/src/resolvers/createActivityResolver.js
+++ create-activity/src/resolvers/createActivityResolver.js
@@ -4,11 +4,18 @@ const dynamoDBClient = require('../database/dynamoDBClient');
 module.exports = {
   Mutation: {
     createActivity: async (_, { name, location, date }) => {
-      const activity = { id: uuidv4(), name, location, date };
+      const activity = {
+        id: uuidv4(),
+        name,
+        location,
+        date,
+      };
+
       await dynamoDBClient.put({
-        TableName: 'Activities',
+        TableName: 'Activities', 
         Item: activity,
       }).promise();
+
       return activity;
     },
   },
