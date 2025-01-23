706c4f35e7f7eea820829766d6d1d8ca61f3bd8a
XavierCuases <xaviercuases@hotmail.com>
2025-01-23 14:22:42 -0500

    feat: :memo: Documentation swagger

--- list-activities/src/index.js
+++ list-activities/src/index.js
@@ -1,28 +1,42 @@
-require('graphql-import-node');
+require("graphql-import-node");
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
+const cors = require("cors");
+const morgan = require("morgan");
+const configureSwagger = require("./swagger");
 const typeDefs = require("./schema/listActivitiesSchema.graphql");
-const resolvers = require("./resolvers/listActivitiesResolver");
-
+const resolvers = require("./resolvers/listActivitiesResolver"); 
 
 const app = express();
+
+app.use(cors());
+
+//app.use(morgan("dev"));
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
-
 server.start().then(() => {
+  
   server.applyMiddleware({ app });
 
-  
+  configureSwagger(app);
+
   app.get("/", (req, res) => {
-    res.send("List activities microservice is running");
+    res.send("Activity creation microservice is running");
   });
 
+  app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send("Something went wrong!");
+  });
 
-  app.listen(4003, () => {
-    console.log("Servidor corriendo en http://localhost:4003/graphql");
+  const PORT = process.env.PORT || 4003;
+
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}/graphql`);
+    console.log(`Swagger available at http://localhost:${PORT}/api-docs`);
   });
 });
