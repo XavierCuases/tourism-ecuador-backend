6ec162d1f7d5550d56bd80e1e188ec1111f14686
XavierCuases <xaviercuases@hotmail.com>
2025-01-22 20:53:51 -0500

    feat: create microservice update activity

---
+++ update-activity/src/resolvers/updateActivityResolver.js
@@ -0,0 +1,44 @@
+const dynamoDBClient = require('../database/dynamoDBClient');
+
+module.exports = {
+  Mutation: {
+    updateActivity: async (_, { id, name, location, date }) => {
+      
+      const getParams = {
+        TableName: 'Activities',
+        Key: { id },
+      };
+
+      const existingItem = await dynamoDBClient.get(getParams).promise();
+      if (!existingItem.Item) {
+        throw new Error(`No activity found with ID: ${id}`); 
+      }
+
+      
+      const updateParams = {
+        TableName: 'Activities',
+        Key: { id },
+        UpdateExpression: `
+          SET ${name ? '#n = :name,' : ''}
+              ${location ? '#l = :location,' : ''}
+              ${date ? '#d = :date,' : ''}
+          `.replace(/,\s*$/, ''), 
+        ExpressionAttributeNames: {
+          ...(name && { '#n': 'name' }),
+          ...(location && { '#l': 'location' }),
+          ...(date && { '#d': 'date' }),
+        },
+        ExpressionAttributeValues: {
+          ...(name && { ':name': name }),
+          ...(location && { ':location': location }),
+          ...(date && { ':date': date }),
+        },
+        ReturnValues: 'ALL_NEW',
+      };
+
+      
+      const result = await dynamoDBClient.update(updateParams).promise();
+      return result.Attributes;
+    },
+  },
+};
