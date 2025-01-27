87e8e7b868707695efd68e3607b9c8edf2c12116
XavierCuases <xaviercuases@hotmail.com>
2025-01-26 22:48:36 -0500

    feat: :sparkles: integrating cors

--- Login-Register-Service/register-user-service/index.js
+++ Login-Register-Service/register-user-service/index.js
@@ -1,12 +1,22 @@
+const express = require('express');
+const cors = require('cors');
 const { app, sequelize } = require('./src/app');
 
 const PORT = 3000;
 
+// Configuración básica de CORS
+app.use(cors({
+    origin: 'http://localhost:3000', // Cambia esto al dominio que necesites
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
+}));
+
+// Conectar la base de datos y levantar el servidor
 sequelize.sync()
     .then(() => {
-        console.log('Base de datos conectada');
+        console.log('Connected database');
         app.listen(PORT, () => {
-            console.log(`Servidor corriendo en http://localhost:${PORT}`);
+            console.log(`Server running in http://localhost:${PORT}`);
         });
     })
-    .catch(error => console.error('Error al conectar con la base de datos:', error));
+    .catch(error => console.error('Error connecting to the database:', error));
--- Login-Register-Service/register-user-service/src/controllers/UserController.js
+++ Login-Register-Service/register-user-service/src/controllers/UserController.js
@@ -5,21 +5,18 @@ exports.registerUser = async (req, res) => {
     try {
         const { name, email, password } = req.body;
 
-        // Validar datos básicos
+
         if (!name || !email || !password) {
             return res.status(400).json({ error: 'All fields are required' });
         }
 
-        // Verificar si el email ya está registrado
         const existingUser = await User.findOne({ where: { email } });
         if (existingUser) {
             return res.status(400).json({ error: 'The email is already registered' });
         }
 
-        // Encriptar la contraseña
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        // Crear usuario
         const newUser = await User.create({
             name,
             email,
