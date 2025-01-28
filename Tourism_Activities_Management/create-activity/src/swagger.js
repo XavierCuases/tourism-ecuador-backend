ef20778c4a6f25cda609abda027934dca97023a1
XavierCuases <xaviercuases@hotmail.com>
2025-01-28 17:03:30 -0500

    feat: :fire: code change

--- Tourism_Activities_Management/create-activity/src/swagger.js
+++ Tourism_Activities_Management/create-activity/src/swagger.js
@@ -1,25 +1,10 @@
-const swaggerJsDoc = require("swagger-jsdoc");
-const swaggerUi = require("swagger-ui-express");
+const path = require('path');
+const yaml = require('yamljs');
+const swaggerUi = require('swagger-ui-express');
 
-const swaggerOptions = {
-  swaggerDefinition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Microservice - Create Activity",
-      version: "1.0.0",
-      description: "API to manage the creation of activities",
-    },
-    servers: [
-      {
-        url: "http://localhost:4001", 
-      },
-    ],
-  },
-  apis: ["./src/resolvers/*.js"], 
-};
-
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
+// Carga el archivo YAML de Swagger
+const swaggerDocument = yaml.load(path.join(__dirname, './docs/swagger.yml'));
 
 module.exports = (app) => {
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 };
