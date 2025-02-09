15b18f36c1b365d7b34162d9cd3df536c5516f0c
XavierCuases <xaviercuases@hotmail.com>
2025-02-09 01:40:31 -0500

    feat: :coffin: changue code en docker file

--- Route-Management/create-route/src/test/java/com/example/create_route/CreateRouteApplicationTests.java
+++ Route-Management/create-route/src/test/java/com/example/create_route/CreateRouteApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.create_route;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest (classes = CreateRouteApplication.class)
 class CreateRouteApplicationTests {
 
 	@Test
