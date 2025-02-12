5eef6c0412c3003b6df2b3d0d90f24ab0ae58892
XavierCuases <xaviercuases@hotmail.com>
2025-02-11 21:29:15 -0500

    feat: :fire: Update Tourism_Activities_Managemen domain

--- Tourism_Activities_Management/create-activity/src/index.js
+++ Tourism_Activities_Management/create-activity/src/index.js
@@ -22,7 +22,7 @@ server.start().then(() => {
   server.applyMiddleware({ app });
 
   app.get('/', (req, res) => {
-    res.send('The Create activities microservice is running 14');
+    res.send('The Create activities microservice is running 13');
   });
 
   const PORT = process.env.PORT || 4001;
