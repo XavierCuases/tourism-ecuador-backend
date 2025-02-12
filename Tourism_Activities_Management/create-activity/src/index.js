d7796a70d05d6542d436c730606dc34b31c2fa20
XavierCuases <xaviercuases@hotmail.com>
2025-02-11 21:53:00 -0500

    feat: :fire: test ci cd

--- Tourism_Activities_Management/create-activity/src/index.js
+++ Tourism_Activities_Management/create-activity/src/index.js
@@ -22,7 +22,7 @@ server.start().then(() => {
   server.applyMiddleware({ app });
 
   app.get('/', (req, res) => {
-    res.send('The Create activities microservice is running 17');
+    res.send('The Create activities microservice is running 18');
   });
 
   const PORT = process.env.PORT || 4001;
