d360ca5778e04031f9c77054693f60618557af9e
XavierCuases <xaviercuases@hotmail.com>
2025-02-12 09:37:52 -0500

    feat: :fire: change controller domain users

--- User_Management/list-users/src/controllers/userController.js
+++ User_Management/list-users/src/controllers/userController.js
@@ -1,9 +1,22 @@
 const { User } = require('../models/userModel');
+
 exports.listUsers = async (req, res) => {
     try {
-        const users = await User.findAll();  
-        res.status(200).json(users); 
+        const userId = req.params.id;  
+        if (userId) {
+          
+            const user = await User.findOne({ where: { id: userId } });
+
+            if (!user) {
+                return res.status(404).json({ message: "User not found" });
+            }
+            return res.status(200).json(user); 
+        } else {
+            
+            const users = await User.findAll();
+            return res.status(200).json(users); 
+        }
     } catch (error) {
-        res.status(500).json({ message: "Error retrieving users", error });
+        return res.status(500).json({ message: "Error retrieving users", error });
     }
 };
--- User_Management/list-users/src/routes/userRoutes.js
+++ User_Management/list-users/src/routes/userRoutes.js
@@ -3,6 +3,9 @@ const userController = require('../controllers/userController');
 
 const router = express.Router();
 
+
 router.get('/users', userController.listUsers);
+
+router.get('/users/:id', userController.listUsers);  
 
 module.exports = router;
