825b4b361141cf3e2a7bf605e92a695deb6eb358
XavierCuases <xaviercuases@hotmail.com>
2025-02-12 16:59:18 -0500

    feat: :sparkles: test 1 routes

--- Route-Management/create-route/src/main/java/com/example/create_route/CreateRouteApplication.java
+++ Route-Management/create-route/src/main/java/com/example/create_route/CreateRouteApplication.java
@@ -6,13 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CreateRouteApplication {
 
     public static void main(String[] args) {
-        System.setProperty("SERVER_PORT", System.getenv().getOrDefault("SERVER_PORT", "5005"));
-        System.setProperty("DB_HOST", System.getenv().getOrDefault("DB_HOST", "users.c20pppmbajcg.us-east-1.rds.amazonaws.com  "));
-        System.setProperty("DB_PORT", System.getenv().getOrDefault("DB_PORT", "5432"));
-        System.setProperty("DB_NAME", System.getenv().getOrDefault("DB_NAME", "routes"));
-        System.setProperty("DB_USERNAME", System.getenv().getOrDefault("DB_USERNAME", "postgres"));
-        System.setProperty("DB_PASSWORD", System.getenv().getOrDefault("DB_PASSWORD", "Federer.-"));
-
-        SpringApplication.run(CreateRouteApplication.class, args);
+     SpringApplication.run(CreateRouteApplication.class, args);
     }
 }
