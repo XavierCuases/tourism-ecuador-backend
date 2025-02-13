2a0c89eccda0f1c315a407cdcc1f9cd32971632b
XavierCuases <xaviercuases@hotmail.com>
2025-02-13 00:15:27 -0500

    feat: :sparkles: Test 1

--- Reservations_Management/cancel-reservation/config.py
+++ Reservations_Management/cancel-reservation/config.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@" \
-                               f"{os.getenv('DB_HOST')}:{os.getenv('DB_PORT', 3306)}/{os.getenv('DB_NAME')}"
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{os.getenv('DB_USER_RE')}:{os.getenv('DB_PASSWORD_RE')}@" \
+                              f"{os.getenv('DB_HOST_RE')}:{os.getenv('DB_PORT_RE', 3306)}/{os.getenv('DB_NAME_RE')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv("SECRET_KEY", "super_secret_key")
-    DEBUG = os.getenv("DEBUG", "false").lower() == "true"
+    SECRET_KEY = os.getenv("SECRET_KEY_US", "super_secret_key")
+    DEBUG = os.getenv("DEBUG_RE", "false").lower() == "true"
--- Reservations_Management/create-reserve/config.py
+++ Reservations_Management/create-reserve/config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_CONFIG = {
-    "host": os.getenv("DB_HOST"),
-    "user": os.getenv("DB_USER"),
-    "password": os.getenv("DB_PASSWORD"),
-    "database": os.getenv("DB_NAME"),
-    "port": int(os.getenv("DB_PORT", 3306))
+    "host": os.getenv("DB_HOST_RE"),
+    "user": os.getenv("DB_USER_RE"),
+    "password": os.getenv("DB_PASSWORD_RE"),
+    "database": os.getenv("DB_NAME_RE"),
+    "port": int(os.getenv("DB_PORT_RE", 3306))
 }
 
 APP_CONFIG = {
-    "debug": os.getenv("DEBUG", "false").lower() == "true",
-    "secret_key": os.getenv("SECRET_KEY", "mysecretkey")
+    "debug": os.getenv("DEBUG_RE", "false").lower() == "true",
+    "secret_key": os.getenv("SECRET_KEY_US", "mysecretkey")
 }
