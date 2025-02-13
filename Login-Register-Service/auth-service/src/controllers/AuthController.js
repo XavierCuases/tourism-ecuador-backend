15e3454cab9a097b275783107884ec3237b93030
XavierCuases <xaviercuases@hotmail.com>
2025-02-13 13:06:27 -0500

    feat: :fire: controller configuration change

--- Login-Register-Service/auth-service/src/controllers/AuthController.js
+++ Login-Register-Service/auth-service/src/controllers/AuthController.js
@@ -13,12 +13,13 @@ exports.login = async (req, res) => {
 
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
-            return res.status(401).json({ message: 'Invalid credentials1' });
+            return res.status(401).json({ message: 'Invalid credentials' });
         }
+        
         const token = jwt.sign(
             { id: user.id, email: user.email, role: user.role }, 
             process.env.JWT_SECRET_US, 
-            { expiresIn: '1h' } 
+            { expiresIn: '1h' }
         );
 
         res.json({
@@ -29,5 +30,40 @@ exports.login = async (req, res) => {
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Internal Server Error' });
+    }
+};
+
+exports.updateUser = async (req, res) => {
+    const { id } = req.params;
+    const { name, email, password } = req.body;
+
+    try {
+        let user = await User.findByPk(id);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        user.name = name || user.name;
+        user.email = email || user.email;
+        if (password) {
+            user.password = await bcrypt.hash(password, 10);
+        }
+
+        await user.save();
+
+        const newToken = jwt.sign(
+            { id: user.id, email: user.email, role: user.role },
+            process.env.JWT_SECRET_US,
+            { expiresIn: '1h' }
+        );
+
+        res.json({
+            message: 'User updated successfully',
+            token: newToken, 
+            role: user.role,
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error updating the user' });
     }
 };
--- Login-Register-Service/auth-service/src/routes/ProtectedRoutes.js
+++ Login-Register-Service/auth-service/src/routes/ProtectedRoutes.js
@@ -2,21 +2,19 @@ const express = require('express');
 const { verifyToken, verifyRole } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
-// Route accessible only to admins
+
 router.get('/admin-dashboard', verifyToken, verifyRole(['admin']), (req, res) => {
-    console.log('Access granted to admin-dashboard'); // Log de acceso
+    console.log('Access granted to admin-dashboard'); 
     res.json({ message: 'Welcome to the admin dashboard' });
 });
 
-// Route accessible only to users
 router.get('/tourist-locations', verifyToken, verifyRole(['user']), (req, res) => {
-    console.log('Access granted to tourist-locations'); // Log de acceso
+    console.log('Access granted to tourist-locations'); 
     res.json({ message: 'Welcome to the tourist locations' });
 });
 
-// Route accessible to both admins and users
 router.get('/profile', verifyToken, (req, res) => {
-    console.log('Access granted to profile'); // Log de acceso
+    console.log('Access granted to profile'); 
     res.json({ message: 'Welcome to your profile', user: req.user });
 });
 
