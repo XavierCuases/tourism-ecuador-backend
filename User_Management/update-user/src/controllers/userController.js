5e1e89638cab140e6837da5a2d2567d0cfb1458f
XavierCuases <xaviercuases@hotmail.com>
2025-02-13 15:45:46 -0500

    feat:

--- User_Management/update-user/src/controllers/userController.js
+++ User_Management/update-user/src/controllers/userController.js
@@ -1,6 +1,5 @@
 const { User } = require('../models/UserModel');
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 
 exports.updateUser = async (req, res) => {
     const { id } = req.params;
