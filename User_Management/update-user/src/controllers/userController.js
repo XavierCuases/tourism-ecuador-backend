325bebe7e9274b65dd9d601437b9a5032df9288b
XavierCuases <xaviercuases@hotmail.com>
2025-02-13 14:03:46 -0500

    feat: :fire: controller configuration change

--- User_Management/update-user/src/controllers/userController.js
+++ User_Management/update-user/src/controllers/userController.js
@@ -1,22 +1,15 @@
 const bcrypt = require('bcryptjs');
 const { User } = require('../models/userModel');
 
-exports.listUsers = async (req, res) => {
-    try {
-        const users = await User.findAll();
-        res.status(200).json(users);
-    } catch (error) {
-        res.status(500).json({ message: "Error retrieving users", error });
-    }
-};
-
 exports.updateUser = async (req, res) => {
     const { id } = req.params;
     const { name, email, password, role } = req.body;
 
     try {
+       
         const user = await User.findOne({ where: { id } });
 
+        
         if (!user) {
             return res.status(404).json({ message: "User not found" });
         }
@@ -26,15 +19,18 @@ exports.updateUser = async (req, res) => {
             return res.status(400).json({ message: "Email is already in use" });
         }
 
+        
         let hashedPassword = password;
         if (password) {
             hashedPassword = await bcrypt.hash(password, 10);
         }
-
+        
         await user.update({ name, email, password: hashedPassword, role });
 
+     
         res.status(200).json({ message: "User updated successfully", user });
     } catch (error) {
+        
         res.status(500).json({ message: "Error updating user", error });
     }
 };
