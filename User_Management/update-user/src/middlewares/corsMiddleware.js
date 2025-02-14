466179a5b7a4c96931362af06534607262029fe8
XavierCuases <xaviercuases@hotmail.com>
2025-02-14 16:47:56 -0500

    feat:

--- User_Management/update-user/src/middlewares/corsMiddleware.js
+++ User_Management/update-user/src/middlewares/corsMiddleware.js
@@ -1,6 +1,6 @@
 const cors = require('cors');
 const corsMiddleware = cors({
-    origin: 'http://localhost:8080', 
+    origin: 'http://3.84.48.248:8080/', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
 });
 
